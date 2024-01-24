41cfa5e76a08a1f67d6dd978e24a67b51b177027
Kevin Duarte <elite.kevin.duarte@gmail.com>
2024-01-24 14:21:55 -0300

    Commit de teste de branch

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, BrowserRouter} from "react-router-dom"
+import { Route, BrowserRouter } from "react-router-dom"
 import Home from "./Home";
 import QuemSomos from "./QuemSomos"
 import DoeAqui from "./DoeAqui"
@@ -10,16 +10,16 @@ import Consultoria from "./Consultoria"
 import FaleConosco from "./FaleConosco"
 
 const Routes = () => {
-    return(
+    return (
         <BrowserRouter>
-            <Route Component = {Home} path="/" exact />
-            <Route Component = {QuemSomos} path="/" exact />
-            <Route Component = {DoeAqui} path="/" exact />
-            <Route Component = {SejaVoluntario} path="/" exact />
-            <Route Component = {Cursos} path="/" exact />
-            <Route Component = {Projetos} path="/" exact />
-            <Route Component = {Consultoria} path="/" exact />
-            <Route Component = {FaleConosco} path="/" exact />
+            <Route Component={Home} path="/" exact />
+            <Route Component={QuemSomos} path="/" exact />
+            <Route Component={DoeAqui} path="/" exact />
+            <Route Component={SejaVoluntario} path="/" exact />
+            <Route Component={Cursos} path="/" exact />
+            <Route Component={Projetos} path="/" exact />
+            <Route Component={Consultoria} path="/" exact />
+            <Route Component={FaleConosco} path="/" exact />
         </BrowserRouter>
     )
 

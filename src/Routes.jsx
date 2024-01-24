7e14417fb2b3591a6a532108651e683587286d15
Kevin Duarte <elite.kevin.duarte@gmail.com>
2024-01-24 14:17:16 -0300

    Adicionado o componente de rotas

---
+++ src/Routes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Route, BrowserRouter} from "react-router-dom"
+import Home from "./Home";
+import QuemSomos from "./QuemSomos"
+import DoeAqui from "./DoeAqui"
+import SejaVoluntario from "./SejaVoluntario"
+import Cursos from "./Cursos"
+import Projetos from "./Projetos"
+import Consultoria from "./Consultoria"
+import FaleConosco from "./FaleConosco"
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Route Component = {Home} path="/" exact />
+            <Route Component = {QuemSomos} path="/" exact />
+            <Route Component = {DoeAqui} path="/" exact />
+            <Route Component = {SejaVoluntario} path="/" exact />
+            <Route Component = {Cursos} path="/" exact />
+            <Route Component = {Projetos} path="/" exact />
+            <Route Component = {Consultoria} path="/" exact />
+            <Route Component = {FaleConosco} path="/" exact />
+        </BrowserRouter>
+    )
+
+}
+
+export default Routes;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Set from './Set.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Principal from './Home.jsx';
+import Home from './Home.jsx';
+import { Route } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Principal/>
+    <Home/>
   </React.StrictMode>,
 )
+
+export default function Main(){
+  return(
+    <Routes/>
+  );
+}

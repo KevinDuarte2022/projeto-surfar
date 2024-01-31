ff3f63be870bb1f713eef4a2f3c1201b7638361e
Kevin Duarte <elite.kevin.duarte@gmail.com>
2024-01-31 16:41:57 -0300

    Botão de doação adicionado

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,28 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import Set from "./Set"
 import NavBarComponent from "./NavBar"
 import './index.css';
 import Carrossel from "./Carrossel"
 import { Container } from "bootstrap-4-react/lib/components/layout";
-import HorizontalExample from "./DoarHome";
-
-
-
+import { useNavigate } from "react-router-dom";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function Home() {
+    let navigate = useNavigate();
+  const doar10 = (string) => {
+    navigate('/doe-aqui', {valor: string});
+  };
     return (
         <div>
             <NavBarComponent />
             <Carrossel />
-            <HorizontalExample/>
+            <ListGroup horizontal className='doarHome'>
+                <ListGroup.Item onClick={() => doar10("Dado a ser enviado")}>R$ 10</ListGroup.Item>
+                <ListGroup.Item>R$30</ListGroup.Item>
+                <ListGroup.Item>R$50</ListGroup.Item>
+                <ListGroup.Item>Outro valor</ListGroup.Item>
+            </ListGroup>
         </div>
-
-
     )
 };
 

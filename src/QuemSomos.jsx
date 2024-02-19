4f0c8c8853b7f739626c07798b5c60fa93b6f8a7
Kevin Duarte <elite.kevin.duarte@gmail.com>
2024-02-19 16:20:39 -0300

    Todas as imagens e textos adicionados na aba 'Quem somos'.

--- src/QuemSomos.jsx
+++ src/QuemSomos.jsx
@@ -1,7 +1,13 @@
 import NavBarComponent from "./NavBar";
 import ImagemQuemSomos from './img/Quem somos nós.png';
 import ImagemP1 from './img/Design sem nome (9).png';
-import ImgagemP2 from './img/Design sem nome (6).png';
+import ImagemP2 from './img/Design sem nome (6).png';
+import ImagemP3 from './img/Design sem nome (3).png';
+import ImagemP4 from './img/CapoeiraEJiuJitsu.png';
+import ImagemP5 from './img/CORTE E COSTURA.png';
+import ImagemP6 from './img/fila.png';
+import ImagemP7 from './img/mãos.png';
+import ImagemP8 from './img/homemSentado.png';
 
 function QuemSomos() {
     return (
@@ -11,7 +17,8 @@ function QuemSomos() {
             <div className="paragrafoQuemSomos">
                 <p>
                     <h3 className="tituloParagrafo">Como tudo começou</h3>
-                    Após sofrerem assalto por dois adolescentes no ano de 2000, o casal de empresários Gicele Melo e Gustavo Rocha tiveram suas vidas transformadas. Um episódio que poderia ter deixado eles com medo e raiva, mas ao contrário, gerou vontade de ajudar e a partir daí , surgiu o Projeto Surfar.
+
+                    Após sofrerem assalto por dois adolescentes no ano de 2000, o casal de empresários Gicele Melo e Gustavo Rocha tiveram suas vidas transformadas. Um episódio que poderia ter deixado eles com medo e raiva, mas ao contrário, gerou vontade de ajudar e a partir daí, surgiu o Projeto Surfar.
                     <img className="imgQuemSomosP" src={ImagemP1} />
 
 
@@ -20,38 +27,56 @@ function QuemSomos() {
                 <p>
                     <h3 className="tituloParagrafo">No Ano de 2006 nasce a Associação Projeto Surfar</h3>
 
-                    Uma Instituição sem fins lucrativos , inicialmente voltada para jovens de periferia em vulnerabilidade social. <br></br>
-                    MISSÃO : Fazer com que as pessoas em vulnerabilidade social melhorem a sua qualidade de vida e alcancem o desenvolvimento sócio econômico. <br></br> VISÃO : Ser referência na inclusão social de pessoas em vulnerabilidade, ofertando projetos de educação, esporte, cultura, lazer e profissionalizantes.<br></br>
+                    Uma Instituição sem fins lucrativos, inicialmente voltada para jovens de periferia em vulnerabilidade social. <br></br>
+                    MISSÃO: Fazer com que as pessoas em vulnerabilidade social melhorem a sua qualidade de vida e alcancem o desenvolvimento sócio econômico. <br></br> VISÃO: Ser referência na inclusão social de pessoas em vulnerabilidade, ofertando projetos de educação, esporte, cultura, lazer e profissionalizantes.<br></br>
                     VALORES: Valorização das pessoas; Comprometimento com a causa social; Qualidade nos resultados.<br></br>
-                    Em 2006 o início tiveram 80 pessoas beneficiadas, 04 voluntários, 05 apoiadores oficinas de alfabetização, reforço escolar e capoeira.
-                    <img src={ImgagemP2} className="imgQuemSomosP"/>
+                    Em 2006 o início tiveram 80 pessoas beneficiadas, 04 voluntários, 05 apoiadores, oficinas de alfabetização, reforço escolar e capoeira.
+                    <img src={ImagemP2} className="imgQuemSomosP" />
                 </p>
-                
-                    
-                
+
+
+
                 <p>
-                    No ano de 2022 ,estamos completando 22 anos de existência 1500 Pessoas beneficiadas 08 Oficinas: Capoeira, surf, Jiu jitsu, alfabetização, reforço escolar, música culinária, Skate 09 Comunidades atendida. A Associação Projeto Surfar esta situada na Rua Borborema ,687 e 691 , na vila João Pessoa - Porto Alegre/RS
-                </p>
-                <p>
-                    A OSC está registrada no CMDCA -PoA/RS,com Serviço de Apoio a Rede de Atendimento , executa o atendimento direto e indireto durante os dias da semana no turno inverso a escola com atividades voltadas para educação ,saúde ,esporte ,cultura e lazer para beneficiados de 0 a 14 anos, tendo como braço forte a Escola de capoeira "Berimabalndo Sonhos " e o Jiu Jitsu .
-                </p>
-                <p>
-                    Cursos profissionalizantes: Para os jovens e os responsáveis dos beneficiados ,são oferecidos cursos visando a inclusão no mercado de trabalho .
+                    <h3 className="tituloParagrafo">24 anos Projeto Surfar</h3>
+
+                    No ano de 2024, estamos completando 24 anos de existência. Com 1500 Pessoas beneficiadas e 08 Oficinas: Capoeira, surf, Jiu jitsu, alfabetização, reforço escolar, música culinária, Skate. 09 Comunidades atendidas. A Associação Projeto Surfar está situada na Rua Borborema, 687 e 691, na vila João Pessoa - Porto Alegre/RS.
+                    <img src={ImagemP3} className="imgQuemSomosP" />
                 </p>
                 <p>
-                    ATENDIMENTOS EMERGENCIAIS DIANTE DA PANDEMIA DO COVID -19 O número de atendimentos cresceu significativamente ,cadastramos mais 600 famílias ,do qual fizemos o atendimento com a entrega de doações de hortifrutis ,refeições (almoço e janta),agasalhos , cobertores ,cestas básicas de alimentos e higiene . Nossa equipe multidisciplinar também atuou de acordo com as necessidades ,oferecndo atendimentos psicológico e psiquiátrico, assistência social ,odontológico e nutricional . CESTAS BÁSICAS DE ALIMENTOS : 1.500/MÊS
+                    <h3 className="tituloParagrafo">Registro e mais</h3>
+
+                    A OSC está registrada no CMDCA - PoA/RS, com Serviço de Apoio a Rede de Atendimento. Executa o atendimento direto e indireto durante os dias da semana no turno inverso a escola com atividades voltadas para educação, saúde, esporte, cultura e lazer para beneficiados de 0 a 14 anos, tendo como braço forte a Escola de capoeira "Berimabal dos Sonhos" e o Jiu Jitsu.
+                    <img src={ImagemP4} className="imgQuemSomosP" />
                 </p>
                 <p>
-                    Cestas básicas de higiene : 1.500/mês doações de hortifrutis: 800 kilos /mês marmitas : 6.000 /mês cobertores : 3.000 /meses inverno agasalhos e calçados : 20.000 peças /ano parceiros : 30 tempo de atuação : 2 anos
+                   <h3 className="tituloParagrafo">Cursos profissionalizantes</h3> 
+
+                    Para os jovens e os responsáveis dos beneficiados, são oferecidos cursos visando a inclusão no mercado de trabalho.
+                    <img src={ImagemP5} className="imgQuemSomosP" />
                 </p>
                 <p>
-                    PARCERIA ATUAL: CEASA RS -PRATO PARTA TODOS ASPOA -ASSOCIAÇÃO DE SURF DE PORTO ALEGRE GR- GUSTAVO ROCHA PRANCHAS DE SURF UNIMAQUI-MANUTENÇÃO DE MÁQUINAS SURFARI - CONFECÇÃO DE VESTUÁRIO E PESQUISAS SOBRE O SURF A.CAPOEIRA -MESTRE GOROROBA PROJETO GADITAS -JIU JITSU BEE SURF WAX -PARAFINAS COMERCIO LOCAL NOTA FISCAL GAÚCHA -PROGRAMA DO GOVERNO TAMPINHA LEGAL - MATERIAS RECICLÁVEIS SIMPATIZANTES DA CAUSA SOCIAL - APOIADORES COMUNIDADES PARCEIRAS - OSCS COMUNIDADE
+                    <h3 className="tituloParagrafo">Atendimentos Emergenciais diante da pandemia do Covid-19</h3>
+
+                    O número de atendimentos cresceu significativamente, cadastramos mais 600 famílias, do qual fizemos o atendimento com a entrega de doações de hortifrutis, refeições (almoço e janta), agasalhos, cobertores, cestas básicas de alimentos e higiene. Nossa equipe multidisciplinar também atuou de acordo com as necessidades,oferecndo atendimentos psicológico e psiquiátrico, assistência social, odontológico e nutricional. Cesta básica de alimentos: 1.500/MÊS. Cestas básicas de higiene: 1.500/mês. Doações de hortifrutis: 800 kilos /mês. Marmitas: 6.000 /mês. Cobertores: 3.000/mês. No inverno, agasalhos e calçados: 20.000 peças/ano. Parceiros: 30. Tempo de atuação: 2 anos.
+                    <img src={ImagemP6} className="imgQuemSomosP" />
                 </p>
                 <p>
-                    Ainda em 2022 pretendemos terminar a construção da escola profissionalizante e ter um melhor aproveitamento dos espaços para iniciar o ano de 2023 com maior organização ,podendo assim ,atender nossos beneficiados com maior qualidade . Com esse novo espaço, poderemos atender simultaneamente os beneficiados com os diversos cursos e atividades . Nossa meta para 2023 é tirar o máximo de chefes de famílias da situação de desemprego ,inserindo-os no mercado de trabalho e impactando assim, na qualidade de vida de toda a família . Nosso sonho é que consigamos evadir a pobreza e a miséria da favela !
+                    <h3 className="tituloParagrafo">Parcerias atuais</h3>
+                    CEASA RS - PRATO PARTA TODOS ASPOA - ASSOCIAÇÃO DE SURF DE PORTO ALEGRE GR - GUSTAVO ROCHA PRANCHAS DE SURF UNIMAQUI-MANUTENÇÃO DE MÁQUINAS SURFARI - CONFECÇÃO DE VESTUÁRIO E PESQUISAS SOBRE O SURF A.CAPOEIRA - MESTRE GOROROBA PROJETO GADITAS - JIU JITSU BEE SURF WAX - PARAFINAS COMERCIO LOCAL NOTA FISCAL GAÚCHA - PROGRAMA DO GOVERNO TAMPINHA LEGAL - MATERIAS RECICLÁVEIS SIMPATIZANTES DA CAUSA SOCIAL - APOIADORES COMUNIDADES PARCEIRAS - OSCS COMUNIDADE.
+                    <img src={ImagemP7} className="imgQuemSomosP"/>
                 </p>
                 <p>
-                    AGRADECEMOS E APROVEITAMOS PARA CONVIDÁ-LOS A VIR SURFAR COM A GENTE E FAZER A DIFERENÇA ! "Se mais pessoas surfarem e e stiverem conectadas com esses valores, o mundo será um lugar melhor" CONTATO: Associação Projeto Surfar -PoA/RS F: 51- 3384 9728 /51-98401 7604 email :ongsurfar@hotmail.com facebook: associação Projeto Surfar Instagran :@projetosurfar
+                    <h3 className="tituloParagrafo">Metas para os próximos anos</h3>
+
+                    Nossa meta para os próximos anos é tirar o máximo de chefes de famílias da situação de desemprego, inserindo-os no mercado de trabalho e impactando, assim, na qualidade de vida de toda a família. Nosso sonho é que consigamos evadir a pobreza e a miséria da favela!
+                    <img src={ImagemP8} className="imgQuemSomosP" />
+                </p>
+                <p>
+                    AGRADECEMOS E APROVEITAMOS PARA CONVIDÁ-LOS A VIR SURFAR COM A GENTE E FAZER A DIFERENÇA! "Se mais pessoas surfarem e estiverem conectadas com esses valores, o mundo será um lugar melhor"<br></br>
+                    CONTATO: Associação Projeto Surfar -PoA/RS F: 51- 3384 9728 /51-98401 7604<br></br>
+                    email :ongsurfar@hotmail.com<br></br>
+                    facebook: associação Projeto Surfar<br></br>
+                    Instagran :@projetosurfar
                 </p>
             </div>
         </div>
